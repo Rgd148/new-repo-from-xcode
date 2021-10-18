9799e5aab1c54479d597697c089f2ef5113f64d9
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-10-18 10:04:12 +0300

    string

--- F/ViewController.swift
+++ F/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 class ViewController: UIViewController {
 var num = 33
     var arr = ["aziz","salem"]
+    var m = "r"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
