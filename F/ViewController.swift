1c2c50b708ad001dec840f394929c6bd94424687
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-10-18 10:05:57 +0300

    good

--- F/ViewController.swift
+++ F/ViewController.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var f = "great"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
