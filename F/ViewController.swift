83a7e4d42128379d5d755e5250ed40effb9f8196
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-10-18 09:56:27 +0300

    add array

--- F/ViewController.swift
+++ F/ViewController.swift
@@ -8,6 +8,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var arr = ["aziz","salem"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
